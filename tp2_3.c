2d262454442e147b7b558a80e9c5ed033825ff9d
Juan <jbcb_09@hotmail.com>
2024-04-15 11:11:06 -0300

    reimplemento cod con aritmetica de punteros

---
+++ tp2_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 5
+#define M 7
+
+int main()
+{
+    int i, j;
+    int mt[N][M];
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *mt[i * 1 + j] = 1 + rand() % 100;
+            printf("%.2d ", *mt[i * 1 + j]);
+        }
+        printf("\n");
+    }
+}
